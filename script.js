c6ff53cf05f397aac56d3a7effb32906b02f1a8d
Nick Marucci <npm1514@gmail.com>
2016-08-08 15:35:52 -0600

    styled page

--- script.js
+++ script.js
@@ -1,38 +1,39 @@
 //determines view block
-var width = 2000,
-    height = 2000;
+var width = 700,
+    height = 700;
 
 //orthographic creates sperical globe
 var projection = d3.geo.orthographic()
-    .scale(500)//determines zoom. We may be able to use this for a function, on click, zoom button
+    .scale(300)//determines zoom. We may be able to use this for a function, on click, zoom button
 //determines position relative to view block
-    .translate([width / 3, height / 3])
+    .translate([width / 2, height / 2])
     .clipAngle(90);
 
 var path = d3.geo.path()
     .projection(projection);
 
 //rotate range
-var λ = d3.scale.linear()
+var a = d3.scale.linear()
     .domain([0, width])
     .range([-180, 180]);
 
-var φ = d3.scale.linear()
+var b = d3.scale.linear()
     .domain([0, height])
     .range([90, -90]);
 
 //viewport svg
-var svg = d3.select("body").append("svg")
+var svg = d3.select(".globe")
+    .append("svg")
     .attr("width", width)
     .attr("height", height);
 
 svg.on("mousedown",function(){
   svg.on("mousemove", function() {
     var p = d3.mouse(this);
-    projection.rotate([λ(p[0]), φ(p[1])]);
+    projection.rotate([a(p[0]), b(p[1])]);
     svg.selectAll("path").attr("d", path);
     svg.on("mouseup", function(){
-      projection.rotate([λ, φ]);
+      projection.rotate([a, b]);
       svg.selectAll("path").attr("d", path);
     });
   });
@@ -46,8 +47,9 @@ svg.on("mousedown",function(){
 d3.json("world-110m.json", function(error, world) {
   if (error) throw error;
 
-  svg.append("path")
-      .datum(topojson.feature(world, world.objects.land))
+  svg.datum(topojson.feature(world, world.objects.land))
+      .append("path")
+      .attr("fill", "rgb(111, 158, 89)")
       .attr("class", "land")
       .attr("d", path);
 });
